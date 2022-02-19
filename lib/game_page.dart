716d49382ec613e0168d4058bcef1d495d398cda
Stanisław Góra <stasgora@gmail.com>
2022-02-19 11:44:17 +0100

    Keyboard input rollback

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,22 +1,24 @@
-import 'package:doslownie/widgets/keyboard.dart';
-import 'package:doslownie/widgets/letter_cell.dart';
+import 'widgets/keyboard.dart';
+import 'widgets/letter_cell.dart';
+import 'package:flutter/services.dart';
 
 import 'logic/grid_cubit.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class GamePage extends StatefulWidget {
-  @override
-  State<GamePage> createState() => _GamePageState();
-}
+class GamePage extends StatelessWidget {
+  final _focusNode = FocusNode();
 
-class _GamePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
-    var widget = BlocBuilder<GridCubit, GridState>(
-      builder: (context, state) =>
-          Center(
+    FocusScope.of(context).requestFocus(_focusNode);
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: RawKeyboardListener(
+        focusNode: _focusNode,
+        child: BlocBuilder<GridCubit, GridState>(
+          builder: (context, state) => Center(
             child: Center(
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
@@ -33,10 +35,23 @@ class _GamePageState extends State<GamePage> {
               ]),
             ),
           ),
+        ),
+        onKey: (event) => _onKeyEvent(event, context),
+      )
     );
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      body: widget,
-    );
+  }
+
+  void _onKeyEvent(RawKeyEvent event, BuildContext context) {
+    var cubit = context.read<GridCubit>();
+    if (event is RawKeyDownEvent) {
+      var letter = event.character;
+      if (letter != null && RegExp(r'[a-zA-Z]').hasMatch(letter)) {
+        cubit.letter(letter.toLowerCase());
+      } else if (event.isKeyPressed(LogicalKeyboardKey.enter)) {
+        cubit.confirm();
+      } else if (event.isKeyPressed(LogicalKeyboardKey.backspace)) {
+        cubit.clear();
+      }
+    }
   }
 }
