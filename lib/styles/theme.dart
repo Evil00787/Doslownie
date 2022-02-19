b142d1d680af0b7e4b74e58413c2a55dd54e8ed6
Evil00787 <matititi123@gmail.com>
2022-02-19 11:43:58 +0100

    - deleted unused methods

--- lib/styles/theme.dart
+++ lib/styles/theme.dart
@@ -1,4 +1,3 @@
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 class CustomTheme {
@@ -13,15 +12,13 @@ class CustomTheme {
     return _customTheme;
   }
 
-
-
   ThemeData getLightTheme(ColorScheme? lightDynamic) {
     ColorScheme colorScheme = const ColorScheme.light();
     if (lightDynamic != null) {
       colorScheme = lightDynamic;
     } else {
       colorScheme = colorScheme.copyWith(
-        primary: Colors.blueGrey, onPrimary: Colors.white30, secondary: Colors.indigoAccent, onSecondary: Colors.white30, surfaceVariant: Colors.grey, surface: Colors.white, onSurfaceVariant: Colors.white,
+        primary: Colors.blueGrey, onPrimary: Colors.white54, secondary: Colors.indigoAccent, onSecondary: Colors.black54, surfaceVariant: Colors.grey, surface: Colors.white, onSurfaceVariant: Colors.black54,
       );
     }
     return ThemeData(
@@ -35,19 +32,11 @@ class CustomTheme {
       darkColorScheme = darkDynamic;
     } else {
       darkColorScheme = darkColorScheme.copyWith(
-        primary: Colors.blueGrey, onPrimary: Colors.white30, secondary: Colors.indigoAccent, onSecondary: Colors.white30, surfaceVariant: Colors.white24, surface: Colors.black26, onSurfaceVariant: Colors.black26
+        primary: Colors.blueGrey, onPrimary: Colors.white54, secondary: Colors.indigoAccent, onSecondary: Colors.white60, surfaceVariant: Colors.white24, surface: Colors.black26, onSurfaceVariant: Colors.white60
       );
     }
     return ThemeData(
       colorScheme: darkColorScheme, useMaterial3: true, fontFamily: "RobotoMono"
     );
-  }
-
-  Color getDynamicGreen() {
-    return Colors.green.harmonizeWith(Theme.of(context).colorScheme.primary);
-  }
-
-  Color getDynamicRed() {
-    return Colors.red.harmonizeWith(Theme.of(context).colorScheme.primary);
   }
 }
