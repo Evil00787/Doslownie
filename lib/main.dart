4449e6f1fab606ff8f70482d742813f9d83a2b9f
Stanisław Góra <stasgora@gmail.com>
2022-02-21 22:33:07 +0100

    Localization support

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'dart:math';
-
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
 
@@ -10,7 +9,8 @@ import 'game_page.dart';
 import 'logic/game_config_cubit.dart';
 import 'logic/grid/grid_cubit.dart';
 import 'logic/keyboard_cubit.dart';
-import 'logic/word_repository.dart';
+import 'services/app_locales.dart';
+import 'services/word_repository.dart';
 import 'styles/theme.dart';
 
 void main() {
@@ -19,21 +19,33 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> implements CurrentLocaleObserver {
   @override
   Widget build(BuildContext context) {
     return DynamicColorBuilder(builder: (
       ColorScheme? lightDynamic,
       ColorScheme? darkDynamic,
     ) {
-      var size = Point<int>(5, 6);
       return MaterialApp(
         title: 'Dosłownie',
         theme: CustomTheme(context).getLightTheme(lightDynamic),
         darkTheme: CustomTheme(context).getDarkTheme(darkDynamic),
         themeMode: ThemeMode.dark,
+
+        localizationsDelegates: const [
+          AppLocales.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: AppLocalesDelegate.supportedLocales,
         home: _withCubit(
           _withCubit(
             _withCubit(
@@ -53,5 +65,14 @@ class MyApp extends StatelessWidget {
       create: cubit,
       child: widget,
     );
+  }
+
+  @override
+  void onLocaleSet(Locale locale) => setState(() {});
+
+  @override
+  void initState() {
+    AppLocales.instance.observeLocaleChanges(this);
+    super.initState();
   }
 }
---
+++ lib/services/app_locales.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+
+import 'package:intl/message_format.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+typedef TranslateFunc = String Function(BuildContext);
+
+// ignore: one_member_abstracts
+abstract class CurrentLocaleObserver {
+  void onLocaleSet(Locale locale);
+}
+
+class AppLocalesDelegate extends LocalizationsDelegate<AppLocales> {
+	static List<Locale> supportedLocales = [
+		const Locale('en', 'US'),
+		const Locale('pl', 'PL')
+	];
+
+	const AppLocalesDelegate();
+
+	@override
+	bool isSupported(Locale locale) {
+		return supportedLocales.contains(locale);
+	}
+
+	@override
+	Future<AppLocales> load(Locale locale) async {
+		await AppLocales.instance.setLocale(locale);
+		return AppLocales.instance;
+	}
+
+	@override
+	bool shouldReload(AppLocalesDelegate old) => false;
+}
+
+class AppLocales {
+	Locale? locale;
+  final List<CurrentLocaleObserver> _localeObservers = [];
+
+	static AppLocales instance = AppLocales();
+  static AppLocales get I => instance;
+	static AppLocales of(BuildContext context) => Localizations.of<AppLocales>(context, AppLocales)!;
+
+	static const LocalizationsDelegate<AppLocales> delegate = AppLocalesDelegate();
+
+	final Map<Locale, Map<String, dynamic>> _translations = {};
+
+	Future setLocale(Locale locale) async {
+		if (_translations.isEmpty) await loadLocales();
+		if (this.locale == locale) return;
+		this.locale = locale;
+    _localeObservers.forEach((observer) => observer.onLocaleSet(locale));
+	}
+
+	Future loadLocales() async {
+		for (var locale in AppLocalesDelegate.supportedLocales) {
+			var localeTranslations = await rootBundle.loadString('i18n/$locale.json');
+			_translations[locale] = json.decode(localeTranslations);
+		}
+	}
+
+	String get(String key, [Map<String, Object>? args, Locale? locale]) {
+		locale ??= this.locale;
+		try {
+			var string = key.split('.').fold(_translations[locale], (object, key) => (object as Map)[key]) as String;
+			if (args == null) return string;
+			return MessageFormat(string, locale: locale.toString()).format(args);
+// ignore: avoid_catching_errors
+		} on NoSuchMethodError {
+			print('Key $key has no localized string in language ${locale!.languageCode}');
+			return '';
+// ignore: avoid_catching_errors
+		} on Error catch (e) {
+      print('$e');
+			return '';
+		}
+	}
+
+  String call(String key, [Map<String, Object>? args, Locale? locale]) => get(key, args, locale);
+
+	Map<String, String> getTranslations(String key, [Map<String, Object>? args]) {
+		var translations = <String, String>{};
+		for (var locale in AppLocalesDelegate.supportedLocales) {
+		  translations[locale.languageCode] = get(key, args, locale);
+		}
+		return translations;
+	}
+
+  void observeLocaleChanges(CurrentLocaleObserver observer) => _localeObservers.add(observer);
+}
